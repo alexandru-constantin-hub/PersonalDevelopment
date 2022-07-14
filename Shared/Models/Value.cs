e48dead042d1fda22ca0cbd6b392a2a1dbab0d20
Alexandru Constantin <aconstantin@zmartests.com>
2022-07-14 00:06:18 -0400

    After scaffolding identity

--- Shared/Models/Value.cs
+++ Shared/Models/Value.cs
@@ -15,7 +15,7 @@ namespace PersonalDevelopment.Shared.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         public string? Description { get; set; }
 
